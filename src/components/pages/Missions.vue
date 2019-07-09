4e7799f8bda1640b034e7cb4da8d510cb254c591
Ahmetcan ÖZCAN <ahmetcanozcan7@gmail.com>
2019-07-09 12:07:51 +0300

    Added somethings

--- src/components/pages/Missions.vue
+++ src/components/pages/Missions.vue
@@ -1,5 +1,6 @@
 <template>
   <v-layout pa-3>
+    <Game v-if="activeMission == 'game'" />
     <div style="width:100%;">
       <v-dialog v-model="dialog">
         <v-card>
@@ -13,24 +14,92 @@
         </v-card>
       </v-dialog>
       <v-card>
-        <v-card-title primary-title>
-          <div>
-            <div class="headline">Nedir bu görevler?</div>
-          </div>
-        </v-card-title>
         <v-card-tile>
-          <v-btn @click="dialog = !dialog" block>Kısa yazımızı oku</v-btn>
+          <v-btn @click="dialog = !dialog" block>Nedir bu görevler ?</v-btn>
         </v-card-tile>
+      </v-card>
+      <br />
+      <v-card color="#FFFFEF">
+        <v-layout>
+          <v-flex xs5 pa-2>
+            <img width="100%" height="auto" src="@/assets/qrcode.jpg" />
+          </v-flex>
+          <v-flex xs7>
+            <v-card-title style="transform:translateY(-10px)" primary-title>
+              <div>
+                <div class="headline">Qr Kod</div>
+                <div
+                  class="missions-small-text"
+                >Etraftaki qr kodları bul, 3 yıldıza kadar yıldız kazan</div>
+                <div class="mt-2" style="text-align:center">
+                  <v-rating small background-color="black" dense :value="qrStars" light :length="3"></v-rating>
+                </div>
+              </div>
+            </v-card-title>
+          </v-flex>
+        </v-layout>
+        <v-btn @click="activeMission = 'qrcode'" block>OYNA</v-btn>
+      </v-card>
+
+      <v-card class="mt-4" color="#FFFFEF">
+        <v-layout>
+          <v-flex xs7>
+            <v-card-title style="transform:translateY(-10px)" primary-title>
+              <div>
+                <div class="headline">OYUN</div>
+                <div class="missions-small-text">Oyunu oyna falan filan abijim</div>
+              </div>
+              <div>{{ gameData.gameDone ? '' : 'Tamamlanmadı' }}</div>
+              <div>Score:{{ gameData.highScore}}</div>
+            </v-card-title>
+          </v-flex>
+
+          <v-flex xs5 pa-1>
+            <img width="100%" height="auto" src="http://placekitten.com/200/200" />
+          </v-flex>
+        </v-layout>
+        <v-btn @click="activeMission = 'game'" block>OYNA</v-btn>
       </v-card>
     </div>
   </v-layout>
 </template>
 <script>
+import Game from "../Game";
+
 export default {
   data() {
     return {
-      dialog: false
+      dialog: false,
+      activeMission: ""
     };
+  },
+  components: {
+    Game
+  },
+  computed: {
+    qrStars() {
+      return 2;
+    },
+    gameData() {
+      return {
+        highScore: 4213,
+        gameDone: false
+      };
+    }
   }
 };
 </script>
+<style>
+.missions-small-text {
+  font-size: 12px;
+}
+.missions-section {
+  width: 100%;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  position: fixed;
+  background-color: rgba(0, 128, 0, 0.219);
+  z-index: 44;
+}
+</style>
